9b5d46eb811ea558b4157f0d86e2b0ec6be5ff8c
Maharnab Naha <nahamaharnab11@gmail.com>
2026-08-02 16:27:02 -0500

    Add pronunciation audio control

---
+++ public/site.js
@@ -0,0 +1,19 @@
+const pronunciationButtons = document.querySelectorAll(".pronunciation-audio");
+const canSpeak =
+  "speechSynthesis" in window && "SpeechSynthesisUtterance" in window;
+
+pronunciationButtons.forEach((button) => {
+  if (!canSpeak) {
+    return;
+  }
+
+  button.hidden = false;
+  button.addEventListener("click", () => {
+    window.speechSynthesis.cancel();
+
+    const pronunciation = new SpeechSynthesisUtterance("ma her nub, nah huh");
+    pronunciation.lang = "en-US";
+    pronunciation.rate = 0.75;
+    window.speechSynthesis.speak(pronunciation);
+  });
+});
