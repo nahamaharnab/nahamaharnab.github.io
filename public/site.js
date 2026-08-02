2c22498c7a123cb2a65e8bffe64affd88fa09088
Maharnab Naha <nahamaharnab11@gmail.com>
2026-08-02 16:42:27 -0500

    Use default pronunciation voice

--- public/site.js
+++ public/site.js
@@ -2,44 +2,6 @@ const pronunciationButtons = document.querySelectorAll(".pronunciation-audio");
 const canSpeak =
   "speechSynthesis" in window && "SpeechSynthesisUtterance" in window;
 
-const maleVoiceHints = [
-  "male",
-  "david",
-  "mark",
-  "guy",
-  "ryan",
-  "george",
-  "alex",
-  "daniel",
-  "aaron",
-  "tom",
-  "fred",
-  "ralph",
-  "bruce",
-];
-
-const findPreferredVoice = () => {
-  const englishVoices = window.speechSynthesis
-    .getVoices()
-    .filter((voice) => /^en(?:-|_|$)/i.test(voice.lang));
-
-  for (const hint of maleVoiceHints) {
-    const voice = englishVoices.find((candidate) =>
-      candidate.name.toLowerCase().includes(hint),
-    );
-
-    if (voice) {
-      return voice;
-    }
-  }
-
-  return (
-    englishVoices.find((voice) => /^en(?:-|_)US$/i.test(voice.lang)) ??
-    englishVoices[0] ??
-    null
-  );
-};
-
 pronunciationButtons.forEach((button) => {
   if (!canSpeak) {
     return;
@@ -51,13 +13,7 @@ pronunciationButtons.forEach((button) => {
 
     const pronunciation = new SpeechSynthesisUtterance("ma her nub, nah huh");
     pronunciation.lang = "en-US";
-    pronunciation.pitch = 0.9;
     pronunciation.rate = 0.75;
-
-    const preferredVoice = findPreferredVoice();
-    if (preferredVoice) {
-      pronunciation.voice = preferredVoice;
-    }
 
     window.speechSynthesis.speak(pronunciation);
   });
